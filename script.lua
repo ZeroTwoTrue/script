25d6ce8d844a80f547c918982ebfc5da438aa984
ZeroTwoTrue <69716131+ZeroTwoTrue@users.noreply.github.com>
2022-01-11 21:22:18 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -12,6 +12,17 @@ local SlotNumber = Instance.new("TextBox")
 
 --Properties:
 
+LumberGui.Name = "LumberGui"
+LumberGui.Parent = LoginGui
+LumberGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+LumberGui.ResetOnSpawn = false
+
+LumberFrame.Name = "LumberFrame"
+LumberFrame.Parent = LumberGui
+LumberFrame.BackgroundColor3 = Color3.fromRGB(31, 31, 31)
+LumberFrame.BorderSizePixel = 0
+LumberFrame.Position = UDim2.new(-0.999000013, 0, 0.542743504, 0)
+LumberFrame.Size = UDim2.new(0, 178, 0, 224)
 
 DupeMoney.Name = "DupeMoney"
 DupeMoney.Parent = LumberFrame
